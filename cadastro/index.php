28f1e61f17da7e27c0ed3a55e07532bf110dfc82
heitorPRdev <herohecttorplay@gmail.com>
2024-03-10 22:06:28 -0300

    1.0.0

---
+++ cadastro/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nome = $_POST["nome"] ?? '';
+        $senha = $_POST["senha"]  ?? '';
+    
+    ?>    
+
+
+
+    <header><h1>Cadastrar</h1></header>    
+
+
+    <section>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" value="<?=$nome?>">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" value="<?=$senha?>">
+            <input type="submit" value="Cadastrar">
+        </form>
+        <p>já tem uma conta?<br><a href="/sitema-de-login-com-PHP/login/">login</a></p>
+    </section>
+    
+</body>
+</html>
