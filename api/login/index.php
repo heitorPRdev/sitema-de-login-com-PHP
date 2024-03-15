3281bc78fb23878524944b3dd0daf7586840939e
heitorPRdev <herohecttorplay@gmail.com>
2024-03-14 21:59:31 -0300

    6.2.6

--- api/login/index.php
+++ api/login/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="api\login\style.css">
+    <link rel="stylesheet" href="./api/login/style.css">
     <script
         src="https://code.jquery.com/jquery-3.7.1.js"
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
